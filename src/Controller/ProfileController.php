3de713bd6841da9b9247f143a65710a2a72d8f9b
Madina Mourtaza <madina.mtz@gmail.com>
2026-01-08 20:16:50 +0100

    ajout du profil et de lauthentification ajout du profil et de lauthentification

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\ProfileFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/profile')]
+#[IsGranted('ROLE_USER')]
+class ProfileController extends AbstractController
+{
+    #[Route('/', name: 'app_profile_show', methods: ['GET'])]
+    public function show(): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        return $this->render('profile/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, EntityManagerInterface $em): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $form = $this->createForm(ProfileFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Profil mis à jour avec succès !');
+
+            return $this->redirectToRoute('app_profile_show');
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'profileForm' => $form,
+        ]);
+    }
+
+    #[Route('/delete', name: 'app_profile_delete', methods: ['POST'])]
+    public function delete(Request $request, EntityManagerInterface $em): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if ($this->isCsrfTokenValid('delete-profile', $request->request->get('_token'))) {
+            $em->remove($user);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre compte a été supprimé.');
+            return $this->redirectToRoute('app_home');
+        }
+
+        $this->addFlash('error', 'Impossible de supprimer le compte.');
+        return $this->redirectToRoute('app_profile_show');
+    }
+}
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,30 +17,27 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', null, [
+                'label' => 'Email',
+            ])
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'J’accepte les conditions',
                 'mapped' => false,
                 'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+                    new IsTrue(message: 'Vous devez accepter les conditions.'),
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
+                'label' => 'Mot de passe',
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
+                    new NotBlank(message: 'Veuillez entrer un mot de passe'),
+                    new Length(
+                        min: 6,
+                        max: 4096,
+                        minMessage: 'Votre mot de passe doit faire au moins {{ limit }} caractères'
+                    ),
                 ],
             ])
         ;
